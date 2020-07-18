56f2eecc4d070d14bd006c6735d2c8a236e0ed75
Adlan Yandarbiev <adlanyandarbiev@gmail.com>
2020-07-18 00:42:09 -0700

    Install nearby-cities. Implement Geolocation Functionality.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React from 'react';
 import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom'
 
@@ -7,9 +8,21 @@ export default class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
         };
+    }
 
+    componentDidMount() {
+        let coordinates;
+        navigator.geolocation.getCurrentPosition(position => {
+            coordinates =
+                {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+            axios.post('/nearbyCity', coordinates).then(response => {
+                console.log(response.data.name);
+            })
+        })
     }
 
     render(){
--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const bodyParser = require("body-parser");
 const express = require('express');
+const nearbyCities = require("nearby-cities")
 const path = require('path');
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
+app.post('/nearbyCity', (req, res) => {
+    res.send(nearbyCities(req.body)[0]);
+})
 
 const port = Number(process.env.PORT || 4000);
-app.listen(port, function () {  console.log('JSON Server is running')});
+
+app.listen(port, function () {
+    console.log('JSON Server is running')
+});
